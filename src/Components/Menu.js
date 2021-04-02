469d4d487bcb6fcd0ff515ef791baf8055595198
Edwardsoen <70268484+Edwardsoen@users.noreply.github.com>
2021-04-02 18:20:12 +0800

    commit

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'; 
 import {MDCMenu} from '@material/menu';
+import Profile from './profile'
+import Modal from 'bootstrap/js/dist/modal'; 
 
 
 class Menu extends React.Component{
@@ -14,6 +16,7 @@ class Menu extends React.Component{
 
     componentDidMount(){
         let menu = new MDCMenu(document.querySelector('.mdc-menu'));
+     
         // menu.open = true;
     }
 
@@ -26,19 +29,28 @@ class Menu extends React.Component{
 
 
     handleLogout(){
-        
         this.props.isLoggedOut("false"); 
         this.generateSession()
         document.cookie = "sessionid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     }; 
 
+    handleAccount(){
+        let myModal = new Modal(document.getElementById('staticBackdrop'), {})
+        myModal.show(); 
+  
+        
+    }
+
 
 
     render(){
         return(
-            <div class="mdc-menu mdc-menu-surface">
+            <div style = {{height: "inherit", width: "inherit"}}>
+                {/* <Profile></Profile> */}
+            <div class="mdc-menu mdc-menu-surface" style = {{overflow: "auto"}} >
+             
                 <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1">
-                    <li class="mdc-list-item" role="menuitem">
+                    <li class="mdc-list-item" role="menuitem" onClick = {this.handleAccount}>
                     <span class="mdc-list-item__ripple"></span>
                     <span class="mdc-list-item__text">Account</span>
                     </li>
@@ -47,6 +59,7 @@ class Menu extends React.Component{
                     <span class="mdc-list-item__text">Logout</span>
                     </li>
                 </ul>
+            </div>
             </div>
         );
     };
--- src/Components/profile.js
+++ src/Components/profile.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'; 
-
-
+import Modal from 'bootstrap/js/dist/modal'; 
 
 class Profile extends React.Component{
     constructor(props){
@@ -10,28 +9,50 @@ class Profile extends React.Component{
         }
         this.props = props; 
     }
+    componentDidMount(){
 
-    render(){
-        <div>
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
+        console.log(document.getElementById("staticBackdrop"))
+    }
+
+    render(){ 
+        return(
+         <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered modal-xl">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Account</h5>
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">test</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <h1>HELLOO</h1>
-                </div>
-                <div class="modal-footer">
+                  </div>
+                  <div class="modal-body">
+                  <div class="d-flex align-items-start">
+                    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                            <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                            <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                            <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+                        </div>
+                        <div class="tab-content" id="v-pills-tabContent">
+                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><h1>home</h1></div>
+                            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"><h1>profile</h1></div>
+                            <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"><h1>messeges</h1></div>
+                            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"><h1>settings</h1></div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary">Understood</button>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
-        </div>
-        };
+
+
+
+
+
+        )
+    };
 
 
 
